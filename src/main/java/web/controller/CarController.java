9b3d4f63ed3690bba2ab17cacc3f54b37dda8cb1
Anastasia Konovalova <pochtaplay2013@gmail.com>
2022-02-16 02:05:02 +0300

    carController fix context.xml fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,13 +5,19 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import web.model.Car;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
     @GetMapping("/cars")
-    public String getCars(ModelMap model) {
+    public String getCars(ModelMap model) throws ClassNotFoundException, SQLException {
+
         Car car = new Car(123123, "devyatka");
         List<String> messages = new ArrayList<>();
         messages.add("Hello!");
@@ -20,5 +26,12 @@ public class CarController {
         model.addAttribute("messages", messages);
         return "cars";
 
+    }
+    @Resource
+    private DataSource testDS;
+    protected void goGet() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conn = null;
+        conn = DriverManager.getConnection("jdbc:mysql://hostname:port/dbname","username", "password");
     }
 }
