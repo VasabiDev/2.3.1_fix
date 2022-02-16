b66436a42fa57a2acfb351260ee18bc6bfe8cb93
Anastasia Konovalova <pochtaplay2013@gmail.com>
2022-02-16 16:16:58 +0300

    counting add

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,34 +3,33 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
+    List<Car> carsList = new ArrayList<>();
     @GetMapping("/cars")
-    public String getCars(ModelMap model) {
-        List<Car> carsList = new ArrayList<>();
+    public String getCars(ModelMap model,@RequestParam(value = "count") Optional<Integer> count) {
+
         carsList.add(new Car(183,"devyatka"));
         carsList.add(new Car(173,"devyatka"));
         carsList.add(new Car(193,"devyatka"));
-//        Car car = new Car(145, "devyatka");
-//        Car car1 = new Car(145, "devyatka");
-//        Car car2 = new Car(145, "devyatka");
-//        Car car3 = new Car(145, "devyatka");
-//        Car car4 = new Car(145, "devyatka");
-//        List<Car> carsList = new ArrayList<>();
-//        carsList.add(car);
-//        carsList.add(car1);
-//        carsList.add(car2);
-//        carsList.add(car3);
-//        carsList.add(car4);
-        model.addAttribute("carsList", carsList);
+
+        model.addAttribute("carsList", getCarsCounted(count.orElse(0)));
         return "cars";
 
     }
 
-
+    public List<Car> getCarsCounted(int count) {
+        if (count == 0) {
+            return carsList;
+        }
+        return carsList.stream().limit(count).collect(Collectors.toList());
+    }
 }
